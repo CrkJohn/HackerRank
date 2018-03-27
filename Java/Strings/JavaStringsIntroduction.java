1ce0e264a9d77ec19d334772591ac004fd57ff5d
John David Ibañez <JhonhRge@gmail.com>
2018-03-27 12:41:03 -0500

    Create JavaStringsIntroduction.java

---
+++ Java/Strings/JavaStringsIntroduction.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Primero{
+	 public static void main(String[] args) {
+	        
+	        Scanner sc=new Scanner(System.in);
+	        String A=sc.next();
+	        String B=sc.next();
+	        System.out.println(A.length()+B.length());
+	        System.out.printf("%s\n", (A.compareTo(B)>0) ? "Yes" : "No");
+	        String tmpA = A.substring(0, 1).toUpperCase() + A.substring(1) , 
+                 tmpB = B.substring(0, 1).toUpperCase() + B.substring(1);
+	        System.out.printf("%s %s\n",tmpA,tmpB);
+	  
+	    }
+	
+	
+	
+}
