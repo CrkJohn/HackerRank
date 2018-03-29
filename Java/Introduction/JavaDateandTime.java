0ec742653d5c5b53b3d5ebb30a81ea07c671e9c0
John David Ibañez <JhonhRge@gmail.com>
2018-03-28 22:12:01 -0500

    Update JavaDateandTime.java

--- Java/Introduction/JavaDateandTime.java
+++ Java/Introduction/JavaDateandTime.java
@@ -8,9 +8,9 @@ public class JavaDateandTime {
 	
 	public static String getDay(String day, String month, String year) {
 		String dia;
-		LocalDate ld  = LocalDate.of(year, month, dayOfMonth);
+		LocalDate ld  = LocalDate.of(Integer.parseInt(year),Integer.parseInt(month),Integer.parseInt(day));
 		return ld.getDayOfWeek().toString();
-    }
+       }
 
 	public static void main(String[] args) {
 		  Scanner in = new Scanner(System.in);
